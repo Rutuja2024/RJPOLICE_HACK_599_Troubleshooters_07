cfdb11793ee083012f65a30e77da4df2a762043a
Sharyu <sharayumarwadi11@gmail.com>
2024-01-08 23:43:54 +0530

    System server setup and database connection

--- server/systemServer/main.py
+++ server/systemServer/main.py
@@ -1,1 +1,18 @@
-print("Hello world")
+import os
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, request
+
+load_dotenv()
+
+app = Flask(__name__)
+
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
